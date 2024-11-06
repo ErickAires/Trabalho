97be00e586d8a48e333dae122cd9d21a02ee74cd
ErickAires <erickpitombeira1842@gmail.com>
2024-11-06 19:22:48 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'sistema';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro: " . $e->getMessage());
+}
+?>
